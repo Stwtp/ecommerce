4d9dc3cdc93b822b5d363ff3f1db5ca4bee79ea2
Sestawut <Stwtp@hotmail.co.th>
2026-03-31 14:32:13 +0700

    feat: add ImageUrl property to Product model and update seed data

--- webapi-boilerplate/Data/AppDbContext.cs
+++ webapi-boilerplate/Data/AppDbContext.cs
@@ -16,9 +16,9 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         );
 
         modelBuilder.Entity<Product>().HasData(
-            new Product { Id = 1, Name = "Pikachu", Price = 49.99M, Stock = 50, CategoryId = 1, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" },
-            new Product { Id = 2, Name = "Agumon", Price = 29.99M, Stock = 100, CategoryId = 2, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" },
-            new Product { Id = 3, Name = "Gundam", Price = 199.99M, Stock = 20, CategoryId = 3, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" }
+            new Product { Id = 1, Name = "Pikachu", ImageUrl = "https://dreamtoy.co.th/media/catalog/product/cache/d68a78be757bdca23b769dd800292e8d/7/0/7016276-1.jpg", Price = 49.99M, Stock = 50, CategoryId = 1, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" },
+            new Product { Id = 2, Name = "Agumon", ImageUrl = "https://inwfile.com/s-dc/8dp9yv.jpg", Price = 29.99M, Stock = 100, CategoryId = 2, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" },
+            new Product { Id = 3, Name = "Gundam", ImageUrl = "https://dr.lnwfile.com/_/dr/_raw/0m/qn/ea.jpg", Price = 199.99M, Stock = 20, CategoryId = 3, IsActive = true, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow, UpdatedBy = "System" }
         );
     }
 
---
+++ webapi-boilerplate/Migrations/20260331073149_AddProductImage.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi_boilerplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1740), new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1740) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1740), new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1740) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1740), new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1750) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1860), "https://dreamtoy.co.th/media/catalog/product/cache/d68a78be757bdca23b769dd800292e8d/7/0/7016276-1.jpg", new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1860) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1860), "https://inwfile.com/s-dc/8dp9yv.jpg", new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1870) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1870), "https://dr.lnwfile.com/_/dr/_raw/0m/qn/ea.jpg", new DateTime(2026, 3, 31, 7, 31, 49, 230, DateTimeKind.Utc).AddTicks(1870) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600), new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600), new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600), new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1600) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660), null, new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660), null, new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "ImageUrl", "UpdatedAt" },
+                values: new object[] { new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660), null, new DateTime(2026, 3, 30, 9, 5, 52, 745, DateTimeKind.Utc).AddTicks(1660) });
+        }
+    }
+}
